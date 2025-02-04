3993dd071bf41871cb86f4260e25f9d3ec2d971b
jibril04Afaa <128831088+jibril04Afaa@users.noreply.github.com>
2025-02-03 23:12:05 -0500

    reduced hero footer text size on mobile

--- src/components/HeroFooter.jsx
+++ src/components/HeroFooter.jsx
@@ -5,16 +5,16 @@ const HeroFooter = () => {
     <div>
       {/* Inner Container */}
       <div>
-        <div className='sm:mt-2 text-xl grid grid-cols-2 p-4 items-center gap-3 tablet:flex tablet:justify-around tablet:mt-14 text-2xl'>
+        <div className='sm:mt-2 text-lg grid grid-cols-2 p-4 items-center gap-3 tablet:flex tablet:justify-around tablet:mt-14 tablet:text-2xl'>
         {/* Years of Experience (YOE) */}
           <div className='flex gap-1'>
             {/* Number */}
             <div>
-              <p className='font-bold text-4xl'>1+</p>
+              <p className='font-bold text-2xl tablet:text-4xl'>1+</p>
             </div>
             {/* Text */}
             <div>
-              <p className='font-bold text-md'>Years of <br /> Experience</p>
+              <p className='font-bold text-sm tablet:text-md'>Years of <br /> Experience</p>
             </div>
           </div>
           
@@ -22,11 +22,11 @@ const HeroFooter = () => {
           <div className='flex gap-1'>
             {/* Number */}
             <div>
-              <p className='font-bold text-4xl'>5+</p>
+              <p className='font-bold text-2xl tablet:text-4xl'>5+</p>
             </div>
             {/* Text */}
             <div>
-              <p className='font-bold text-md'>Projects <br />Completed</p>
+              <p className='font-bold text-sm tablet:text-md'>Projects <br />Completed</p>
             </div>
           </div>
 
@@ -34,11 +34,11 @@ const HeroFooter = () => {
           <div className='flex gap-1'>
             {/* Number */}
             <div>
-              <p className='font-bold text-4xl' >1+</p>
+              <p className='font-bold text-2xl tablet:text-4xl'>1+</p>
             </div>
             {/* Text */}
             <div>
-              <p className='font-bold text-md'>Research<br /> Completed</p>
+              <p className='font-bold text-sm tablet:text-md'>Research<br /> Completed</p>
             </div>
           </div>
 
