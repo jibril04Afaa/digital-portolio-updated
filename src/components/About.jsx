6c718852931a1ff6bdcd9f4f17e7a89552f67f8a
jibril04Afaa <128831088+jibril04Afaa@users.noreply.github.com>
2025-02-05 23:19:00 -0500

    working on About.jsx component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,8 +3,13 @@ import React from 'react'
 const About = () => {
   return (
     <div>
-      <div>
-        <div></div>
+      <div className='border-2 container mx-auto px-6 py-8'>
+        {/* Title */}
+        <div className='flex justify-center items-center'>
+          <p className='font-bold text-3xl'>About Me</p>
+        </div>
+
+        {/* */}
       </div>
     </div>
   )
