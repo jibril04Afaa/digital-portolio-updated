2e01e241299efe63353e9b7b9d2bb01e8f8ea3bc
jibril04Afaa <128831088+jibril04Afaa@users.noreply.github.com>
2025-02-06 02:54:43 -0500

    fixed mobile screen navbar issue in prod

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,43 +14,30 @@ import { FaMoon } from "react-icons/fa"
 const Navbar = () => {
   return (
     <>
-      <Router>
-        {/* Hide navbar completely on mobile, show on tablet and above */}
-        <div className="hidden tablet:block">
-          <div className="flex justify-between items-center p-4">
-            
-            {/* Logo */}
-            <div>
-              <p>JB</p>
-            </div>
+      {/* Hide navbar below tablet (650px) */}
+      <div className="hidden tablet:block">
+        <div className="flex justify-between items-center p-4">
+          
+          {/* Logo */}
+          <div>
+            <p>JB</p>
+          </div>
 
-            {/* Navigation Items */}
-            <nav className="flex gap-10 p-3 rounded-3xl cursor-pointer">
-              <Link to="/" className="">Home</Link>
-              <Link to="/about">About</Link>
-              <Link to="/skills">Skills</Link>
-              <Link to="/projects">Projects</Link>
-              <Link to="/contact">Contact</Link>
-            </nav>
+          {/* Navigation Items */}
+          <nav className="flex gap-10 p-3 rounded-3xl cursor-pointer">
+            <Link to="/" className="">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/skills">Skills</Link>
+            <Link to="/projects">Projects</Link>
+            <Link to="/contact">Contact</Link>
+          </nav>
 
-            {/* Light/dark mode button */}
-            <div>
-              <FaMoon className="rounded-full p-1 text-4xl"/>
-            </div>
+          {/* Light/dark mode button */}
+          <div>
+            <FaMoon className="rounded-full p-1 text-4xl"/>
           </div>
         </div>
-
-        <div>
-          <Routes>
-            <Route path="/" element={<Hero/>}/> 
-            <Route path="/about" element={<About/>}/>
-            <Route path="/skills" element={<Skills/>}/>
-            <Route path="/projects" element={<Projects/>}/>
-            <Route path="/contact" element={<Contact/>}/> 
-            <Route path="*" element={<ErrorPage/>}/> {/* Error Route */}
-          </Routes>
-        </div>
-      </Router>
+      </div>
     </>
   )
 }
